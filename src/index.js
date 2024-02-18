5bcabd2a5959c1f832a19d51e2c13c1d3493a8d8
David Cuartas <84470267+David-CuartasC@users.noreply.github.com>
2024-02-17 23:34:23 -0500

    Diseño html y CSS

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const boton = document.querySelector('#boton');
+    const menu = document.querySelector('#menu');
+
+    boton.addEventListener('click', () => {
+        menu.classList.toggle('hidden')
+        menu.classList.toggle('flex')
+    })
+})
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html',],
+  theme: {
+    fontFamily: {
+      outfit: ['Outfit',' sans-serif']
+    },
+    extend: {
+      colors: {
+        cl1: '#205D55',
+        cl2: '#705117',
+        cl3: '#DFDCDC'
+      }
+    },
+  },
+  plugins: [],
+}
